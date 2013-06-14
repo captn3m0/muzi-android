3698f2410a8435a180e6d58e9ac27ba099e10597
Shivam Mangla <shivam.mangla.09@gmail.com>
2013-06-15 00:33:16 +0530

    Remove search button - Add search functionality to keyboard enter key. - Change default icon for search from android device. - Can be custom labelled as well if required.

--- src/muzi/sdslabs/co/in/HomeScreen.java
+++ src/muzi/sdslabs/co/in/HomeScreen.java
@@ -3,26 +3,26 @@ package muzi.sdslabs.co.in;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
-public class HomeScreen extends Activity implements OnItemClickListener,
-		OnClickListener {
+public class HomeScreen extends Activity implements OnItemClickListener {
 
 	ListView lv;
 	ArrayAdapter<String> adapter;
-	String listItems[] = { "Albums", "Artists", "Top Tracks",
-			"Top Albums", "Language" };
+	String listItems[] = { "Albums", "Artists", "Top Tracks", "Top Albums",
+			"Language" };
 	char stringlist[] = new char[26];
-	ImageButton ibSearch;
 	EditText etSearch;
 
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,8 +31,6 @@ public class HomeScreen extends Activity implements OnItemClickListener,
 
 		lv = (ListView) findViewById(R.id.lvHomeScreen);
 		lv.setOnItemClickListener(HomeScreen.this);
-		ibSearch = (ImageButton) findViewById(R.id.ibSearchInHomeScreen);
-		ibSearch.setOnClickListener(HomeScreen.this);
 		etSearch = (EditText) findViewById(R.id.etSearchBoxHomeScreen);
 
 		adapter = new ArrayAdapter<String>(this,
@@ -44,6 +42,29 @@ public class HomeScreen extends Activity implements OnItemClickListener,
 		for (int i = 0; i < 25; i++) {
 			stringlist[i] = (char) (65 + i); // String.valueOf(Character.toChars(i))
 		}
+
+		etSearch.setOnEditorActionListener(new OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+				boolean handled = false;
+				if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+					if (etSearch.getText().toString().length() < 3) {
+						Toast.makeText(HomeScreen.this,
+								"Enter minimum of 3 characters to search",
+								Toast.LENGTH_SHORT).show();
+					} else {
+						Intent i = new Intent(HomeScreen.this,
+								SearchResults.class);
+						i.putExtra("search_query", etSearch.getText()
+								.toString());
+						startActivity(i);
+					}
+					handled = true;
+				}
+				return handled;
+			}
+		});
 	}
 
 	@Override
@@ -58,31 +79,18 @@ public class HomeScreen extends Activity implements OnItemClickListener,
 		// TODO Auto-generated method stub
 
 		if (av.getId() == R.id.lvHomeScreen) {
-			try {
-				Intent i = new Intent(HomeScreen.this,
-						FilteredListAfterQuery.class);
-				i.putExtra("filter_type", listItems[position]);
-				// Log.i("extra", listItems[position]);
-				startActivity(i);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		if (v.getId() == R.id.ibSearchInHomeScreen) {
-
-			if (etSearch.getText().toString().length() < 3) {
-				Toast.makeText(HomeScreen.this,
-						"Enter minimum of 3 characters to search",
-						Toast.LENGTH_SHORT).show();
-			} else {
-				Intent i = new Intent(HomeScreen.this, SearchResults.class);
-				i.putExtra("search_query", etSearch.getText().toString());
-				startActivity(i);
+			// if clicked on artist or album
+			if (position == 1 || position == 0) {
+				try {
+					Intent i = new Intent(HomeScreen.this,
+							FilteredListAfterQuery.class);
+					i.putExtra("filter_type", listItems[position]);
+					// Log.i("extra", listItems[position]);
+					startActivity(i);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
