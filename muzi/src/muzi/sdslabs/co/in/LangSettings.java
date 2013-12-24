d0490df50e84fe74e09939017701ac7c05149477
Shivam <shivam.mangla.09@gmail.com>
2013-12-24 00:28:29 -0800

    Don't show back button in Settings. Clean code in LangSettings.java

--- muzi/src/muzi/sdslabs/co/in/LangSettings.java
+++ muzi/src/muzi/sdslabs/co/in/LangSettings.java
@@ -1,15 +1,12 @@
 package muzi.sdslabs.co.in;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ToggleButton;
 
 import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.MenuItem;
 
 public class LangSettings extends SherlockActivity {
 
@@ -36,24 +33,6 @@ public class LangSettings extends SherlockActivity {
 
 	}
 
-	public void onToggleClicked(View view) {
-		/*
-		 * Sample code to implement on click for future
-		 */
-		//
-		// if (view.getId() == R.id.tbEnglish) {
-		// boolean on = ((ToggleButton) view).isChecked();
-		//
-		// if (on) {
-		// Toast.makeText(getApplicationContext(), "English on",
-		// Toast.LENGTH_SHORT).show();
-		// } else {
-		// Toast.makeText(getApplicationContext(), "English off",
-		// Toast.LENGTH_SHORT).show();
-		// }
-		// }
-	}
-
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
@@ -67,15 +46,5 @@ public class LangSettings extends SherlockActivity {
 		Log.i("Hindi", pref.getBoolean("Hindi", true) + "");
 		Log.i("Tamil", pref.getBoolean("Tamil", true) + "");
 		LangSettings.this.finish();
-	}
-
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == android.R.id.home) {
-			Intent mainIntent = new Intent(getApplicationContext(),
-					HomeScreen.class);
-			mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			startActivity(mainIntent);
-		}
-		return true;
 	}
 }
