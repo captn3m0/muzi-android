8138df6a053286b43690e6db5d6d72fcaa5b2994
Shivam <shivam.mangla.09@gmail.com>
2013-12-26 23:46:57 -0800

    Show toast when some action on widget is reported

--- muzi/src/muzi/sdslabs/co/in/Widget.java
+++ muzi/src/muzi/sdslabs/co/in/Widget.java
@@ -6,41 +6,60 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class Widget extends AppWidgetProvider {
 
 	// This class is called every time the app updates
-	int awID;
-	RemoteViews v;
-	
-	@Override
-	public void onEnabled(Context context) {
-		// TODO Auto-generated method stub
-		super.onEnabled(context);
-	}
+	public static int awID = 100;
+	RemoteViews notiView;
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		// TODO Auto-generated method stub
-		super.onReceive(context, intent);
-	}
+	/**
+	 * Order of calls: Enabled-Received-update-receive when u pick up a widget
+	 * receive-delete when u delete a widget
+	 */
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		// TODO Auto-generated method stub
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		v = new RemoteViews(context.getPackageName(), R.layout.notification);
 		Log.i("WIDGET", "running");
 		// v.setTextViewText(R.id.tvWidget, quote);
-		appWidgetManager.updateAppWidget(awID, v);
+
+		notiView = new RemoteViews(context.getPackageName(),
+				R.layout.notification);
+		notiView.setTextViewText(R.id.ntvTitle, "HAHA HEHE HOHO");
+		appWidgetManager.updateAppWidget(awID, notiView);
 	}
 
 	@Override
-	public void onDeleted(Context context, int[] appWidgetIds) {
+	public void onEnabled(Context context) {
 		// TODO Auto-generated method stub
-		super.onDeleted(context, appWidgetIds);
-		// Toast.makeText(context, "See you soon !!",
-		// Toast.LENGTH_SHORT).show();
+		super.onEnabled(context);
+
+		AppWidgetManager appWidgetManager = AppWidgetManager
+				.getInstance(context);
+		notiView = new RemoteViews(context.getPackageName(),
+				R.layout.notification);
+		notiView.setTextViewText(R.id.ntvTitle, "HAHA HEHE HOHO");
+		appWidgetManager.updateAppWidget(awID, notiView);
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		Log.i("MusicService: DownloadReceiver", intent.getIntExtra("action", 0)
+				+ "received");
+		Toast.makeText(context, intent.getIntExtra("action", 0) + " received",
+				Toast.LENGTH_SHORT).show();
+
+		AppWidgetManager appWidgetManager = AppWidgetManager
+				.getInstance(context);
+		notiView = new RemoteViews(context.getPackageName(),
+				R.layout.notification);
+		notiView.setTextViewText(R.id.ntvTitle, "HAHA HEHE HOHO");
+		appWidgetManager.updateAppWidget(awID, notiView);
+
 	}
 }
